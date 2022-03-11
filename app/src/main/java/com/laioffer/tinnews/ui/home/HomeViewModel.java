bda3146834c6003500268d51fc46eb9e4d5bb642
Fire <56602568+Thewarmsunshine@users.noreply.github.com>
2022-03-11 03:39:27 -0800

    Frist Upload

---
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeViewModel.java
@@ -0,0 +1,34 @@
+package com.laioffer.tinnews.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.laioffer.tinnews.model.Article;
+import com.laioffer.tinnews.model.NewsResponse;
+import com.laioffer.tinnews.repository.NewsRepository;
+
+public class HomeViewModel extends ViewModel {
+
+    private final NewsRepository repository;
+    private final MutableLiveData<String> countryInput = new MutableLiveData<>();
+
+    public HomeViewModel(NewsRepository newsRepository) {
+        this.repository = newsRepository;
+    }
+
+    public void setCountryInput(String country) {
+        countryInput.setValue(country);
+    }
+
+    public void setFavoriteArticleInput(Article article) {
+        repository.favoriteArticle(article);
+    }
+
+    public LiveData<NewsResponse> getTopHeadlines() {
+        //countryInput发生变化，后面也会trigger
+        //return就是第二个参数返回等参数
+        return Transformations.switchMap(countryInput, repository::getTopHeadline);
+    }
+}
